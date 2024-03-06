df3f14db6bef1e905869684ffa9365c8beb71390
tptptp28712 <159972348+tptptp28712@users.noreply.github.com>
2024-03-06 16:38:55 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,22 @@
-
+package Main
+import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+	 String hobbys = scanner.nextLine();
+	 // 昇順or降順
+	 switch(hobbys){
+	 case "昇順":
+	  Ken.ken sub = new Ken.ken();
+			sub.publicMethod();
+	break;
+	case "降順":
+	     Ken.ken2 sub = new Ken.ken2();
+			sub.publicMethod2();
+	break;
+	 }
+    }
+}
